8e67603cd8073f91088a1e166855e7c6af3eda8c
luca rampoldi <luca.rampoldi.it@gmail.com>
2018-08-30 12:09:33 +0200

    added goal description

--- app/controllers/onboarding/goals_controller.rb
+++ app/controllers/onboarding/goals_controller.rb
@@ -7,8 +7,15 @@ class Onboarding::GoalsController < ApplicationController
     @user = User.find(params[:user_id])
     goals = params[:user][:goal_list].drop(1)
     @user.goal_list = goals
-    @user.save!
+    @user.update(user_params)
+
 
     redirect_to user_path(@user)
   end
+end
+
+
+private
+def user_params
+  params.require(:user).permit(:goal_list, :goal_description)
 end

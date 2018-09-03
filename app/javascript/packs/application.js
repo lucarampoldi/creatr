706ff3c7a0fcfacde913db676aa4b55048473769
Andy B <andybarnov@gmail.com>
2018-09-03 17:04:44 +0200

    before master merge

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,5 +4,8 @@ import "awesomplete";
 import "awesomplete/awesomplete.css";
 import { enableTagInput } from "../components/awesomplete";
 import { toggleColor } from "../components/multiInput";
+
+import "../components/index-cards";
+
 enableTagInput();
 toggleColor();

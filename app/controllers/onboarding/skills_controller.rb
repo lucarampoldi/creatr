a52fbdf44007f6f7ca892e642e0479503f505d12
luca rampoldi <luca.rampoldi.it@gmail.com>
2018-09-03 12:11:13 +0200

    done with onboarding form

--- app/controllers/onboarding/skills_controller.rb
+++ app/controllers/onboarding/skills_controller.rb
@@ -5,9 +5,18 @@ class Onboarding::SkillsController < ApplicationController
 
   def update
     @user = User.find(params[:user_id])
-    @user.skill_list.add(params[:user][:skill_list, :linkedin_url, :github_url, :dribbble_url], parse: true)
+    @user.skill_list.add(params[:user], parse: true)
+    @user.update(user_params)
+    raise
     @user.save!
 
     redirect_to onboarding_goals_path
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:linkedin_url, :github_url, :dribbble_url)
+  end
+
 end

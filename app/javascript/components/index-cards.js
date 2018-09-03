6150a1bb458b98df24f84e47de59947195e066f5
Andy B <andybarnov@gmail.com>
2018-09-03 17:11:06 +0200

    remove bad index card

--- app/javascript/components/index-cards.js
+++ app/javascript/components/index-cards.js
@@ -1,14 +1,14 @@
-function revealCard(id) {
-  const firstCard = document.querySelector(`#user_${id}`);
-  const passButton = firstCard.querySelector("a.disliker");
-
-  passButton.addEventListener("click", e => {
-    firstCard.classList.toggle("hidden");
-  });
-
-  firstCard.classList.remove("hidden");
-}
-
-function showNextCard() {}
-
-revealCard(3);
+// function revealCard(id) {
+//   const firstCard = document.querySelector(`#user_${id}`);
+//   const passButton = firstCard.querySelector("a.disliker");
+//
+//   passButton.addEventListener("click", e => {
+//     firstCard.classList.toggle("hidden");
+//   });
+//
+//   firstCard.classList.remove("hidden");
+// }
+//
+// function showNextCard() {}
+//
+// revealCard(3);

78499382ceb4b51913dce7276ffeaf9bafa9acf8
Luisa Schrott <luisa.schrott@gmail.com>
2018-09-04 16:07:35 +0200

    works now on phone

--- db/schema.rb
+++ db/schema.rb
@@ -107,8 +107,6 @@ ActiveRecord::Schema.define(version: 2018_09_03_144327) do
     t.string "linkedin_url"
     t.string "github_url"
     t.string "dribbble_url"
-    t.text "achievement"
-    t.string "goal_description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
